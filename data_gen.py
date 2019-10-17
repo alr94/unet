ed014d5747b033b1a807f28104e4c829281f60e4
Aidan Lewis Reynolds <areynold@tlab-gpu-pdune-02.cern.ch>
2019-10-17 13:56:10 +0200

    More general datagen

--- data_gen.py
+++ data_gen.py
@@ -12,7 +12,7 @@ from losses import *
 
 class DataGenerator(keras.utils.Sequence):
 
-  def __init__(self, root_data, dataset_type, batch_size = 32, shuffle = True, 
+  def __init__(self, root_data, dataset_type, dirname, batch_size = 32, shuffle = True, 
                patch_w = 160, patch_h = 160, patch_depth = 3, val_frac = 0.05,
                test_frac = 0.05):
     
@@ -31,19 +31,25 @@ class DataGenerator(keras.utils.Sequence):
     self.n_crop = int((164 - patch_w)/ 2)
     
     self.TFile = ROOT.TFile(root_data)
-    self.TDir  = self.TFile.Get('MichelEnergyImage')
+    self.TDir  = self.TFile.Get(dirname)
     
-    self.data_types = ['wire', 'cluem', 'clumichel', 'truth', 'trueEnergy', 
-                       'energy']
+    self.data_types = []
+    for k in self.TDir.GetListOfKeys():
+      keyType = k.GetName().split('_')[-1] 
+      if keyType not in self.data_types: self.data_types.append(keyType)
     
-    self.keys = defaultdict()
+    self.keys     = defaultdict()
     for data_type in self.data_types:
       self.keys[data_type] = [k for k in self.TDir.GetListOfKeys()
                               if data_type == k.GetName().split('_')[-1]]
+      
+    self.energies = [k.GetName().split('_')[3][6:] for k in 
+                     self.TDir.GetListOfKeys() if 'wire' == 
+                     k.GetName().split('_')[-1]]
     
-    val_size   = int(np.floor(len(self.keys['truth']) * self.val_frac))
-    test_size  = int(np.floor(len(self.keys['truth']) * self.test_frac))
-    train_size = len(self.keys['truth']) - val_size - test_size
+    val_size   = int(np.floor(len(self.keys['wire']) * self.val_frac))
+    test_size  = int(np.floor(len(self.keys['wire']) * self.test_frac))
+    train_size = len(self.keys['wire']) - val_size - test_size
     
     if self.dataset_type   == 'train':
       for data_type in self.data_types:
@@ -68,6 +74,9 @@ class DataGenerator(keras.utils.Sequence):
         ROOT.SetOwnership(test_hist, True)
         if np.isnan(np.sum(test_array)): nan_index[i] = False
           
+    self.energies = [d for d, s in itertools.izip(self.energies, nan_index) 
+                     if s]
+    
     for data_type in self.data_types:
       self.keys[data_type] =  [d for d, s in 
                                itertools.izip(self.keys[data_type], nan_index) 
@@ -76,7 +85,7 @@ class DataGenerator(keras.utils.Sequence):
     self.on_epoch_end()
     
   def __len__(self):
-    return int(np.floor(len(self.keys['truth']) / self.batch_size))
+    return int(np.floor(len(self.keys['wire']) / self.batch_size))
   
   def __getitem__(self, batch_index):
     
@@ -87,9 +96,35 @@ class DataGenerator(keras.utils.Sequence):
     X, Y = self.__data_generation(key_indexes)
     
     return X, Y
+  
+  def getitembykey(self, batch_index, key):
+    
+    low = batch_index * self.batch_size
+    high = (batch_index + 1) * self.batch_size
+    key_indexes = self.indexes[low:high]
+    
+    X = np.empty((self.batch_size, self.patch_w, self.patch_h, 1))
+    
+    for sample_num, key_index in enumerate(key_indexes):
+      hist = self.keys[key][key_index].ReadObj()
+      X[sample_num,..., 0] = root_numpy.hist2array(hist)[self.n_crop:-self.n_crop , self.n_crop:-self.n_crop]
+      ROOT.SetOwnership(hist, True)
+    
+    return X
+  
+  def getenergy(self, batch_index):
+    
+    low = batch_index * self.batch_size
+    high = (batch_index + 1) * self.batch_size
+    key_indexes = self.indexes[low:high]
+    
+    X = np.empty((self.batch_size, 1))
+    for sample_num, key_index in enumerate(key_indexes):
+      X[sample_num, 0] = self.energies[key_index]
+    return X
     
   def on_epoch_end(self):
-    self.indexes = np.arange(len(self.keys['truth']))
+    self.indexes = np.arange(len(self.keys['wire']))
     if self.shuffle == True:
       np.random.shuffle(self.indexes)
     
@@ -130,8 +165,9 @@ class DataGenerator(keras.utils.Sequence):
         X[sample_num, ..., 0] = root_numpy.hist2array(wire_hist)[self.n_crop:-self.n_crop , self.n_crop:-self.n_crop]
         ROOT.SetOwnership(wire_hist, True)
       
-      truth_hist = self.keys['truth'][key_index].ReadObj()
-      Y[sample_num, ..., 0] = root_numpy.hist2array(truth_hist)[self.n_crop:-self.n_crop , self.n_crop:-self.n_crop]
-      ROOT.SetOwnership(truth_hist, True)
+      if self.dataset_type != 'data':
+        truth_hist = self.keys['truth'][key_index].ReadObj()
+        Y[sample_num, ..., 0] = root_numpy.hist2array(truth_hist)[self.n_crop:-self.n_crop , self.n_crop:-self.n_crop]
+        ROOT.SetOwnership(truth_hist, True)
       
     return X, Y
