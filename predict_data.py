dbb96c813d9068828f783225cfc587215ba42f1d
Aidan Lewis Reynolds <areynold@tlab-gpu-pdune-02.cern.ch>
2019-10-21 16:50:47 +0200

    filter flattened array data

--- predict_data.py
+++ predict_data.py
@@ -42,6 +42,9 @@ import math
 import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
+  
+from ROOT import TFile, TTree
+from array import array
 
 ################################################################################
 # My stuff
@@ -74,7 +77,8 @@ batch_size       = 1
 
 print ('Building data generator')
 # FIXME
-test_gen = DataGenerator(dataset_type = 'data', dirname = 'MichelEnergyImageData', 
+test_gen = DataGenerator(dataset_type = 'data', 
+                         dirname = 'MichelEnergyImageData', 
                          batch_size = batch_size, shuffle = False, 
                          root_data = args.input, patch_w = patch_w, 
                          patch_h = patch_h, patch_depth = n_channels)
@@ -91,48 +95,56 @@ with sess.as_default():
   test_x      = np.zeros((test_gen.__len__(), patch_w, patch_h, n_channels))
   test_y      = np.zeros((test_gen.__len__(), patch_w, patch_h, 1))
   test_charge = np.zeros((test_gen.__len__(), patch_w, patch_h, 1))
+  test_energy = np.zeros((test_gen.__len__(), patch_w, patch_h, 1))
   for i in range(test_gen.__len__()):
     test_x[i], test_y[i] = test_gen.__getitem__(i)
     test_charge[i]       = test_gen.getitembykey(i, 'wire')
-    
-  #FIXME
-  # test_x    = test_x[:8]
-    
+    test_energy[i]       = test_gen.getitembykey(i, 'energy')
+     
   print ('Making predictions')
   predictions = model.predict(test_x, batch_size = 8, verbose = 1)
+  del test_x, test_y
+    
   print ('Made predictions')
-  flat = predictions.flatten()
-  e_flat = test_charge[..., 0].flatten()
+  q_flat = test_charge[..., 0].flatten()
+  flat   = predictions.flatten()[q_flat > 0.1]
+  e_flat = test_energy[..., 0].flatten()[q_flat > 0.1]
+  q_flat = q_flat[q_flat > 0.1]
   
-  from ROOT import TFile, TTree
-  from array import array
+  print (len(flat), len(q_flat))
   
-  # FIXME
-  # f = TFile('performance_' + args.weights.split('/')[-2] + '.root', 'recreate')
-  f = TFile('predict_data.root', 'recreate')
+  f = TFile('predict_output.root', 'recreate')
   
+  ##############################################################################
   t_ev = TTree('performance_tests', 'Thresholded performance scores')
   b_thresh     = array('f', [0.])
   b_inv_thresh = array('f', [0.])
   b_nhi        = array('f', [0.])
   b_rq         = array('f', [0.])
-  t_ev.Branch('thresh'              , b_thresh     , 'b_thresh/F')
-  t_ev.Branch('inv_thresh'          , b_inv_thresh , 'b_inv_thresh/F')
-  t_ev.Branch('n_hits_image'        , b_nhi        , 'b_nhi/F')
-  t_ev.Branch('reco_charge'         , b_rq         , 'b_rq/F')
-  
-  
-  t_hit      = TTree('hit_metrics', 'Thresholded hit scores')
-  b_hit_cnn  = array('f', [0.])
-  b_hit_int  = array('f', [0.])
-  t_hit.Branch('hit_cnn' , b_hit_cnn , 'b_hit_cnn/F')
-  t_hit.Branch('hit_int' , b_hit_int , 'b_hit_int/F')
+  b_re         = array('f', [0.])
+  t_ev.Branch('thresh'       , b_thresh     , 'b_thresh/F')
+  t_ev.Branch('inv_thresh'   , b_inv_thresh , 'b_inv_thresh/F')
+  t_ev.Branch('n_hits_image' , b_nhi        , 'b_nhi/F')
+  t_ev.Branch('reco_charge'  , b_rq         , 'b_rq/F')
+  t_ev.Branch('reco_energy'  , b_re         , 'b_re/F')
   ##############################################################################
-    
+  t_hit = TTree('hit_metrics', 'Thresholded hit scores')
+  b_hit_cnn    = array('f', [0.])
+  b_hit_int    = array('f', [0.])
+  b_hit_energy = array('f', [0.])
+  t_hit.Branch('hit_cnn'    , b_hit_cnn    , 'b_hit_cnn/F')
+  t_hit.Branch('hit_int'    , b_hit_int    , 'b_hit_int/F')
+  t_hit.Branch('hit_energy' , b_hit_energy , 'b_hit_energy/F')
+  ##############################################################################
+  
+  print ('Trees made')
   for i in range(len(flat)):
-    b_hit_cnn[0]  = flat[i]
-    b_hit_int[0]  = e_flat[i]
+    if i % 1000 == 0: print(i, len(flat))
+    b_hit_cnn[0]    = flat[i]
+    b_hit_int[0]    = q_flat[i]
+    b_hit_energy[0] = e_flat[i]
     t_hit.Fill()
+  print ('Hit Tree filled')
   
   threshes = [0.1, 0.3, 0.5, 0.7, 0.9, 1 - 1e-2, 1 - 1e-3, 1 - 1e-4, 1 - 1e-5, 
               1 - 1e-6, 1 - 1e-7]
@@ -140,9 +152,12 @@ with sess.as_default():
   
   for thresh in threshes:
     
+    print ('Thresh', thresh)
+    
     b_thresh[0] = thresh
       
     integratedQs, recoQs = [], []
+    integratedEs, recoEs = [], []
     nHits                = []
     
     for i in range(len(predictions)):
@@ -150,15 +165,20 @@ with sess.as_default():
       nHitImage  = NHits(predictions[i], thresh)
       
       recoQ       = RecoEnergy(predictions[i], test_charge[i], thresh)
+      recoE       = RecoEnergy(predictions[i], test_energy[i], thresh)
       
       b_inv_thresh[0] = 1. / (1. - thresh)
       b_nhi[0]        = nHitImage
       b_rq[0]         = recoQ
+      b_re[0]         = recoE
       
       t_ev.Fill()
       
       recoQs.append(recoQ)
+      recoEs.append(recoE)
       nHits.append(nHitImage)
+      
+  print ('Reco tree filled')
         
   f.Write()
   f.Close()
